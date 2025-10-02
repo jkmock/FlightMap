54e33ceea48874c5e14aae08e32807bec9cb1a45
Jackson Mock <jkmock@ncsu.edu>
2025-10-02 13:46:32 -0400

    [Implementation] Added page to reference embeded code

---
+++ skysouth-demo/src/app/embed/page.js
@@ -0,0 +1,11 @@
+'use client';
+
+import SkySouthFlightsDemo from "../sky_south_flights_demo";
+
+export default function EmbedMap() {
+  return (
+    <div className="h-screen w-screen">
+      <SkySouthFlightsDemo />
+    </div>
+  );
+}
